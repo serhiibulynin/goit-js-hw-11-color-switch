1882653513e9341d45885c9404cebb58d12d57bd
serhiibulynin <81964924+serhiibulynin@users.noreply.github.com>
2021-06-29 14:24:23 +0000

    Deploying to gh-pages from @ serhiibulynin/goit-js-hw-11-color-switch@85efa70eecde3621954a9890488273f01bb08375 🚀

--- src.0d0ac3a4.js
+++ src.0d0ac3a4.js
@@ -1,6 +1,6 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss");const t=["#FFFFFF","#2196F3","#4CAF50","#FF9800","#009688","#795548"],e=document.querySelector("#panel"),a=document.querySelector('[data-action="start"]');let n=null;function r(e){"start"===e.target.dataset.action&&(n=setInterval(()=>{document.body.style.backgroundColor=t[s(0,t.length-1)]},1e3),e.target.disabled=!0),"stop"===e.target.dataset.action&&(clearInterval(n),a.disabled=!1)}e.addEventListener("click",r);const s=(t,e)=>Math.floor(Math.random()*(e-t+1)+t);
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-color-switch/src.7da9692c.js.map
+//# sourceMappingURL=/goit-js-hw-11-color-switch/src.0d0ac3a4.js.map
